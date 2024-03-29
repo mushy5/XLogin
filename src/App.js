e8d3e6f7e9515ad3ae94501a5ff825fd79def415
Shifa <shifa.gadiwale@gmail.com>
2024-03-29 18:04:09 +0530

    Login Functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const [submitted, setSubmitted] = useState(false);
+const [error, setError] = useState('');
+
+const handleSubmit = (e)=>{
+  e.preventDefault();
+  setError('');
+  if(username === 'user' && password ==='password')
+    setSubmitted('true');
+  else
+    setError('Invalid username or password');
+}
+
   return (
     <div className="App">
+     <h1>Login Page</h1>
      
+     <p>{error}</p>
+     {submitted ? `Welcome, ${username}!` 
+     : 
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='username'>Username:</label>
+            <input type='text' required id='username' placeholder='username' value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+          </div>
+
+          <div>
+            <label htmlFor='password'>Username:</label>
+            <input type='password'required id='password' placeholder='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+          </div>
+
+          <button type='submit'>Submit</button>
+        </form>
+     }
+    
     </div>
   );
 }
